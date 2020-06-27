a497103e60678941801d56f72190464e0878a83d
Michael Adams <mikelovesbooks@gmail.com>
2020-06-27 13:37:41 +0100

    Update install-docker-on-ubuntu.sh

--- install-docker-on-ubuntu.sh
+++ install-docker-on-ubuntu.sh
@@ -5,4 +5,6 @@ sudo apt install --yes apt-transport-https ca-certificates curl gnupg2 software-
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - &&
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" &&
 sudo apt update &&
-sudo apt install --yes docker-ce
+sudo apt install --yes docker-ce &&
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+sudo chmod +x /usr/local/bin/docker-compose
